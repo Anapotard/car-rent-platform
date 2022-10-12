a9c4b0efb742235db31a0f6f06a2101335bdc156
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-10-12 17:54:22 +0200

    created views and data

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :brand, :model, :address, presence: true
-  validates_numericality_of :price_per_day, :greater_than => 0
+  validates_numericality_of :price, :greater_than => 0
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating database'
+Booking.destroy_all
+Car.destroy_all
+puts 'Database created'
+
+20.times do
+  Car.create!(
+    user: User.first,
+    brand: Faker::Vehicle.manufacture,
+    model: Faker::Vehicle.make,
+    price: rand(50..400),
+    address: Faker::Address.street_address,
+    year_of_production: rand(1950..2022)
+  )
+end
+
+puts "Done"
