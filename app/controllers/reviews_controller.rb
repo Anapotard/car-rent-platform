fc0a4b3812ac099f7e63a8f9363c8730b4f103e4
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-10-12 19:12:53 +0200

    adding review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :set_car, only: [:new, :create]
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.car = @car
+    if @review.save
+      redirect_to car_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_reservation
+    @car = car.find(params[:car_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  # def create
+  #   @car = car.find(params[:car_id])
+  #   @review = @car.reviews.create(review_params)
+  #   redirect_to car_path(@car)
+  # end
+
+  # private
+
+  # def review_params
+  #   params.require(:review).permit(:content, :rating)
+  # end
 end
